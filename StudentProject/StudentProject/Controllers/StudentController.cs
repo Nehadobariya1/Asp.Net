de0a458ca494ee98f1aa9efead975904d89d45ba
Nehadobariya1 <143496591+Nehadobariya1@users.noreply.github.com>
2024-07-03 18:31:53 +0530

    add Project

---
+++ StudentProject/StudentProject/Controllers/StudentController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StudentProject.Models;
+
+namespace StudentProject.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {   
+            student s = new student();
+            s.Id = 1;
+            s.Name = "Prins";
+            s.Description = "i am student of Master Of Computer Application";
+            s.city = "Rajkot";
+
+            return View(s);
+        }
+        public ActionResult About()
+        {
+            ViewBag.Id = new List<int>()
+            { 1, 2 ,3};
+            ViewBag.Name = new List<string>()
+            {
+                "krish","Meet","Karm"
+            };
+            ViewBag.Discription = new List<string>()
+            {
+                "Student","Student","Employee"
+            };
+            ViewBag.City = new List<string>()
+            {
+                "Rajkot","Jungadh","Mahesana"
+            };
+
+            return View();
+
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ StudentProject/StudentProject/Models/student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentProject.Models
+{
+    public class student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string city { get; set; }
+
+
+    }
+}
