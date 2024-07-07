1331d921f12e8fc5014cf73816b17c5205e80331
Nehadobariya1 <143496591+Nehadobariya1@users.noreply.github.com>
2024-07-07 13:33:13 +0530

    Add changes in project

--- StudentProject/StudentProject/Controllers/StudentController.cs
+++ StudentProject/StudentProject/Controllers/StudentController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml.Linq;
 using StudentProject.Models;
 
 namespace StudentProject.Controllers
@@ -42,7 +43,15 @@ namespace StudentProject.Controllers
         }
         public ActionResult Contact()
         {
-            return View();
+            List<student> std = new List<student>
+            {
+                new student() { Id = 1,Name = "Krish", Description="I am student os MCA",city="Rajkot"},
+                new student() { Id = 2,Name = "prinsh", Description="I am student os MBA",city="jungadh"},
+                new student() { Id = 3,Name = "raj", Description="I am student os MA",city="jamnagar"},
+                new student() { Id = 4,Name = "ravi", Description="I am student os MSC",city="surat"},
+                new student() { Id = 5,Name = "hardik", Description="I am student os M.COM",city="gondal"}
+            };
+            return View(std);
         }
     }
 }
